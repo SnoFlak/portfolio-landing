c6e0e0541e042a5d5f4cf6e755d8d15f6fcc9dc1
Mathias <mathiasasletten@gmail.com>
2022-09-10 12:12:02 -0600

    update complete rework, style change; safety updte

--- portfolio-landing/src/App.js
+++ portfolio-landing/src/App.js
@@ -1,37 +1,27 @@
 import './App.css';
-import Navigation from './components/Navigation';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import SkillsCarousel from './components/SkillsCarousel.js';
 function App() {
   return (
     <div className="App" id="home">
+      {
+        //start fresh, start refreshed, start renewed
+      }
       <header>
-        <div className="header-bar">
-          <div className="left-header">
-            <div className="header-text">
-              <div className="name-title">
-                <h1 className="name-text">Mathias Sletten</h1> 
-                <h1 className="frontend-developer">, Frontend Developer</h1>
-              </div>
-              <p className="info">mathiasasletten@gmail.com (701)-600-9009</p>
-            </div>
-          </div>
+        <div className="left-header">
+          <h1><b><span>this</span>.name=</b>(<span>"Mathias Sletten"</span>);</h1>
+          <p><i><span>this</span>.titles=[<span>"Front End Developer"</span>, <span>"Father"</span>, <span>"Lifelong learner"</span>];</i></p>
         </div>
+        <nav className="right-header">
+          <a href='/'>.about();</a>
+          <a href='/'>.projects();</a>
+          <a href='/'>.contact();</a>
+        </nav>
       </header>
-      <div className="nav-control-div">
-        <Navigation />
+      <div>
+        <div className="stacks">
+          <SkillsCarousel />
+        </div>
       </div>
-      <About />
-      <Projects />
-      <Contact />
-      <div className="back-to-top">
-        <a href="#home">Back to top</a>
-      </div>
-      <footer>
-        <h3>Special Thanks</h3>
-        <p>Special thanks to John Shoff for helping me pursue my dream job, and supporting me all the way through. Thanks to Mathias Begley for helping me dive into programming and helping me out in the toughest of times, and being a great business partner. Thanks to my family, for constantly being supportive of what I want to do in life, and being there on the journey with me.</p>
-      </footer>
     </div>
   );
 }
---
+++ portfolio-landing/src/lib/skillIcons.js
@@ -0,0 +1,19 @@
+import node from "../imgs/node.png";
+import chrome from "../imgs/chrome.png";
+import css3 from "../imgs/css3.png";
+import html5 from "../imgs/html5.png";
+import javascript from "../imgs/js.png";
+import typescript from "../imgs/typescript.png";
+import github from "../imgs/github.png";
+import react from "../imgs/react.png";
+
+export const skillIcons = {
+    node,
+    chrome,
+    css3,
+    html5,
+    javascript,
+    typescript,
+    github,
+    react,
+};
