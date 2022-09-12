5c45227652d9b85ffd31602a7f8b4dbe0f2e5d2c
Mathias <mathiasasletten@gmail.com>
2022-09-12 12:16:45 -0600

    update added links to github projects

--- portfolio-landing/src/components/Projects.js
+++ portfolio-landing/src/components/Projects.js
@@ -17,7 +17,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div className="line-code">
-                    <p id="storefront"><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;let</span> Storefront = <span style={{color: "cyan",}}>"E-Commerce Storefront"</span>;</p>
+                    <a href="https://github.com/SnoFlak/storefront/tree/main/storefront"><p id="storefront"><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;let</span> Storefront = <span style={{color: "cyan",}}>"E-Commerce Storefront"</span>;</p></a>
                 </div>
             </div>
 
@@ -31,7 +31,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div className="line-code">
-                    <p id="taskmeister"><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;let</span> Taskmeister = <span style={{color: "cyan",}}>"Task Calendar"</span>;</p>
+                <a href="https://github.com/SnoFlak/Taskmeister"><p id="taskmeister"><span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;let</span> Taskmeister = <span style={{color: "cyan",}}>"Task Calendar"</span>;</p></a>
                 </div>
             </div>
 
